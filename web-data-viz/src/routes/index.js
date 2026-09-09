9564a8392c81cbd3ee9b6d974c7de653088cb25f
Sabrina-Toledo <sabrina.toledo@sptech.school>
2026-09-09 20:11:12 -0300

    Redirecionando index para home da flixview // ajustes na web-data-viz

--- web-data-viz/src/routes/index.js
+++ web-data-viz/src/routes/index.js
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res) {
-    res.render("index");
+    res.redirect("/html/home.html");
+
 });
 
 module.exports = router;
